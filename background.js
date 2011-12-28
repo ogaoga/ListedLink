572b9c9b4d8454d9d95989e6151d10c36222439e
Tsutomu Ogasawara <info@ogaoga.org>
2011-12-28 15:05:41 +0900

    Removed non needed lines.

--- background.js
+++ background.js
@@ -1,17 +1,16 @@
 /* -*- coding:utf-8 -*- */
 //$(function(){
 // ======================================================================
-  var server = 'http://icotile.ogaoga.org';
   chrome.self.onConnect.addListener(function(port, name) {
     port.onMessage.addListener(function(info, con) {
       if ( info.type == 'getUser' ) {
         var data = sessionStorage.getItem(info.data.screen_name);
         if ( data ) {
-          console.log('*** use cache *** : '+info.data.screen_name);
+          // console.log('*** use cache *** : '+info.data.screen_name);
           port.postMessage(JSON.parse(data));
         }
         else {
-          console.log('!!! not use cache !!! : '+info.data.screen_name);
+          // console.log('!!! not use cache !!! : '+info.data.screen_name);
           var url = 'https://api.twitter.com/1/users/show.json';
           $.ajax({'type': 'GET',
                   'url': url,
@@ -22,7 +21,7 @@
                                            JSON.stringify(data));
                   },
                   'error': function(XMLHttpRequest, textStatus, errorThrown) {
-                    console.log(XMLHttpRequest);
+                    // console.log(XMLHttpRequest);
                     port.postMessage();
                   },
                  });
